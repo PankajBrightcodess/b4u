ca55ec4b5f582e64c38ffbc4a0131c4e07a9d085
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-02-24 18:03:10 +0530

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -111,7 +111,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 
 }else{
 
-	defined('DB_HOST')      ? null : define('DB_HOST', 'Localhost ');
+	defined('DB_HOST')      ? null : define('DB_HOST', 'localhost');
 	defined('DB_USER')      ? null : define('DB_USER', 'softwarebss_bforyou');
 	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'BforU@2022');
 	defined('DB_NAME')      ? null : define('DB_NAME', 'softwarebss_beforu');
