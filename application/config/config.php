446fcf0b6088a32c9f42296b11830b76ce5e96c3
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-02-24 17:51:12 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -33,7 +33,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/b4u/";
 }
 else{
-	$config['base_url'] = "https://$_SERVER[HTTP_HOST]/b4u/";
+	$config['base_url'] = "https://demo.softwarebss.com/b4u/";
 }
 
 
