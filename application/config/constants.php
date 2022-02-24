5f9a67e723928476ad3de787c9df30ae4b62cd04
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-02-24 18:13:59 +0530

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -111,10 +111,10 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 
 }else{
 
-	defined('DB_HOST')      ? null : define('DB_HOST', 'localhost');
-	defined('DB_USER')      ? null : define('DB_USER', 'softwarebss_bforyou');
-	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'BforU@2022');
-	defined('DB_NAME')      ? null : define('DB_NAME', 'softwarebss_beforu');
+	defined('DB_HOST')      ? null : define('DB_HOST', 'Localhost');
+	defined('DB_USER')      ? null : define('DB_USER', 'softwarebss_bforu');
+	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'Bforu@2022#');
+	defined('DB_NAME')      ? null : define('DB_NAME', 'softwarebss_bforu');
 	
 }
 	// elseif(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='software.brightcodess.in'){
