b26269699281abbb74fd93d337494fad781226b4
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-03-09 12:36:46 +0530

    update

--- application/config/constants.php
+++ application/config/constants.php
@@ -112,7 +112,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 }else{
 
 	defined('DB_HOST')      ? null : define('DB_HOST', 'localhost');
-	defined('DB_USER')      ? null : define('DB_USER', 'softwarebss_bazar4you');   
+	defined('DB_USER')      ? null : define('DB_USER', 'softwarebss_bazar4you');
 	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'Bazar4you@123#');
 	defined('DB_NAME')      ? null : define('DB_NAME', 'softwarebss_bazar4you');
 	
